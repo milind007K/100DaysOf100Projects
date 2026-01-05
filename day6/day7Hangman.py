9407798743fb09d73a448aee79df0e2151834fce
learninggit <milindkhorgade07@gmail.com>
2026-01-05 22:26:17 +0530

    Initial commit - Day7Hangman

---
+++ day6/day7Hangman.py
@@ -0,0 +1,33 @@
+word_list = ["aardvark" , "baboon" , "camel"]
+
+import random
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_lenght = len(chosen_word)
+for position in range(word_lenght):
+    placeholder += "_"
+    print(placeholder)
+
+game_over = False
+correct_letter = []
+
+while not game_over:
+    guess = input("guess a letter: ").lower()
+print(guess)
+
+display = ""
+for letter in chosen_word:
+    if letter == guess:
+        display += letter
+        correct_letter.append(letter)
+    elif letter in correct_letter:
+        display += letter
+    else:
+        display += "_"
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("you win !")
